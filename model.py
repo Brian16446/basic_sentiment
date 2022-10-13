2654bab13e49f2293f65b18e39726f159e35bf4b
Brian <Brian.Bridge@ii-solutions.co.uk>
2022-10-13 15:53:02 +0100

    new test

--- model.py
+++ model.py
@@ -1,21 +1,45 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import triton_python_backend_utils as pb_utils
 
-tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
-model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+# tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+# model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
 
-inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
-with torch.no_grad():
-    logits = model(**inputs).logits
+# inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
+# with torch.no_grad():
+#     logits = model(**inputs).logits
 
-predicted_class_id = logits.argmax().item()
-print(model.config.id2label[predicted_class_id])
+# predicted_class_id = logits.argmax().item()
+# print(model.config.id2label[predicted_class_id])
 
 
-class PyTorch_to_TorchScript(torch.nn.Module):
-    def __init__(self):
-        super(PyTorch_to_TorchScript, self).__init__()
+class TritonPythonModel:
+
+    @staticmethod
+    def auto_complete_config(auto_complete_model_config):
+        # OPTIONAL
+        pass
+
+    def initialize(self, args):
+        # OPTIONAL. Called once when model is loaded.
+        print("initialised")
+        self.tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
         self.model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
-    def forward(self, data, attention_mask=None):
-        return self.model(data, attention_mask)
- 
+
+    def execute(self, requests):
+        # Must be implemented. Returns a list of pb_utils.InferenceRequest
+        responses = []
+        for request in requests:
+            query = [t.decode("UTF-8") for t in pb_utils.get_input_tensor_by_name(request, "TEXT").as_numpy().tolist()]
+            tokens = self.tokenizer(text=query[0], return_tensors="pt", return_attention_mask=False)
+            print("tokens generated")
+            with torch.no_grad():
+                logits = self.model(**tokens).logits
+                predicted_class_id = logits.argmax().item()
+                responses.append(pb_utils.InferenceResponse(predicted_class_id))
+
+        return responses
+
+    def finalize(self):
+        # OPTIONAL. Runs on exit. Clean up.
+        print("Cleaning up")
